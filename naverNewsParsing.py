47fd24a1ff33f600538e97c989626668e57b246c
jey25 <jjangeoyoung@gmail.com>
2022-05-13 14:35:54 +0900

    6
    image parsing

--- naverNewsParsing.py
+++ naverNewsParsing.py
@@ -5,5 +5,10 @@ html = urlopen("https://news.naver.com/")
 
 bsObject = BeautifulSoup(html, "html.parser")
 
+#뉴스 링크 가져오기
 for link in bsObject.find_all('a'):
     print(link.text.strip(), link.get('href'))
+
+#뉴스 이미지 가져오기
+for link in bsObject.find_all('img'):
+    print(link.text.strip(), link.get('src'))
