c083ff4b2d50c98666f07bca50d2bf00c9f32644
jey25 <jjangeoyoung@gmail.com>
2023-09-29 16:55:26 +0900

    Update 230925_udemy_re.py

--- 230925_udemy_re.py
+++ 230925_udemy_re.py
@@ -5,4 +5,16 @@ import requests
 response = requests.get("")
 
 soup = BeautifulSoup(response.text, "html.parser")
+tags = soup.find_all(name="a", class_="storylink")
 
+texts = []
+links = []
+
+for tag in tags:
+    text = tag.get_text()
+    texts.append(text)
+    link = tag.get("href")
+    links.append(link)
+
+
+upvote = [score.get_text() for score in soup.find_all(name="span", class_="socre")]
