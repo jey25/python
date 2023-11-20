df744e85cd94698801da828d7c2b39226dd76ec2
Jang eo young <jjangeoyoung@gmail.com>
2023-11-20 18:01:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,54 @@
 
 
-환율 = {"달러": 1167, 
-        "엔": 1.096, 
-        "유로": 1268, 
-        "위안": 171}
-user = input("입력: ")
-num, currency = user.split()
-print(float(num) * 환율[currency], "원")
+# 딕셔너리를 이용한 환율 변환 프로그램
+# 환율 = {"달러": 1167, 
+#         "엔": 1.096, 
+#         "유로": 1268, 
+#         "위안": 171}
+
+# user = input("입력: ")
+# num, currency = user.split()
+# print(float(num) * 환율[currency], "원")
+
+
+# 3개의 숫자를 입력받은 후 가장 큰 숫자 출력
+# number = []
+
+# for _ in range(3):
+#     user_input = int(input("숫자 : "))
+#     number.append(user_input)
+
+# print(max(number))
+
+
+
+# user_input = input("입력 : ")
+# number_list = user_input.split('-')
+
+# if number_list[0] == "011":
+#     com = "skt"
+# elif number_list[0] == "016":
+#     com = "kt"
+# else:
+#     com = "lg"
+
+# print(f"{com}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
