b9e463f0087e45f2d4164b0bdbec4b0fd1ceffc1
Jang eo young <jjangeoyoung@gmail.com>
2023-11-29 17:26:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -148,3 +148,7 @@ price_list = [32100, 32150, 32000, 32500]
 # for i in range( 1, len(my_list)-1 ) :
 #   print(my_list[i-1], my_list[i], my_list[i+1])
 
+my_list = ["가", "나", "다", "라"]
+
+for i in range(len(my_list), 1, -1):
+    print(my_list[i-1], my_list[i-2])
