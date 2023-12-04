ebfe50136b640e31fe91ea05f63aa880efeb7d39
Jang eo young <jjangeoyoung@gmail.com>
2023-12-04 17:46:06 +0900

    Update test.py

--- test.py
+++ test.py
@@ -169,11 +169,15 @@ price_list = [32100, 32150, 32000, 32500]
 
 
 
+# low_prices  = [100, 200, 400, 800, 1000]
+# high_prices = [150, 300, 430, 880, 1000]
 
+# volatility = []
 
+# for i in range(5):
+#     vol = high_prices[i] - low_prices[i]
+#     volatility.append(vol)
 
-
-
-
+# print(volatility)
 
 
