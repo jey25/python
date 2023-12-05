3f5e7e20ca4b612b6cc95433c8da9042533cd0c8
Jang eo young <jjangeoyoung@gmail.com>
2023-12-05 09:07:43 +0900

    update

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+
+from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return render_template('index.html')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
