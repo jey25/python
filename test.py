f5cfb59fc7342d9fbda87a977deb89996b14c5e3
Jang eo young <jjangeoyoung@gmail.com>
2023-12-07 16:45:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -189,17 +189,23 @@
 #         print(col)
 
 
+# 이중 리스트 배열 저장
 # data = [
 #     [ 2000,  3050,  2050,  1980],
 #     [ 7500,  2050,  2050,  1980],
 #     [15450, 15050, 15550, 14900]
 # ]
 
-# result = []
+# result = [
+# ]
 
 # for row in data:
+#     sub = []
 #     for col in row:
-#         result.append(col * 1.00014)
-
+#         sub.append(col * 1.00014)
+#     result.append(sub)
 # print(result)
+
+
+
 
