eb3eb6be81ec53141274ba24359108ce64fa659d
Jang eo young <jjangeoyoung@gmail.com>
2023-12-07 17:04:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -207,12 +207,3 @@
 # print(result)
 
 
-ohlc = [["open", "high", "low", "close"],
-        [100, 110, 70, 100],
-        [200, 210, 180, 190],
-        [300, 310, 300, 310]]
-
-for row in ohlc[1:]:
-    print(row[3])
-        
-
