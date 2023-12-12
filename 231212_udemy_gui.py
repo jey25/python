e02b6a9c2854c144aced774682e91b04464efb11
Jang eo young <jjangeoyoung@gmail.com>
2023-12-12 15:42:44 +0900

    Update 231212_udemy_gui.py

--- 231212_udemy_gui.py
+++ 231212_udemy_gui.py
@@ -6,10 +6,21 @@ window.title("Manager Center")
 window.minsize(width=500, height=300)
 
 wiki_search = tkinter.Label (text="search")
-wiki_search.pack()
+wiki_search.grid(row=0, column=0, padx=10, pady=10)
+
+wiki_search_btn = tkinter.Button (text="Click")
+wiki_search_btn.grid(row=0, column=1, padx=10, pady=10)
 
 
 
+day_off = tkinter.Label (text="day_off")
+day_off.grid(row=0, column=5, padx=10, pady=10)
+
+dev_build = tkinter.Label (text="dev_build")
+dev_build.grid(row=1, column=5, padx=10, pady=10)
+
+find_issue = tkinter.Label (text="find_issue")
+find_issue.grid(row=2, column=5, padx=10, pady=10)
 
 
 
