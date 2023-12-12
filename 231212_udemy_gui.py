397d77b2ee403f99eb89421f9f6dc29396d5dc55
Jang eo young <jjangeoyoung@gmail.com>
2023-12-12 16:11:32 +0900

    Update 231212_udemy_gui.py

--- 231212_udemy_gui.py
+++ 231212_udemy_gui.py
@@ -11,8 +11,6 @@ wiki_search.grid(row=0, column=0, padx=10, pady=10)
 wiki_search_btn = tkinter.Button (text="Click")
 wiki_search_btn.grid(row=0, column=1, padx=10, pady=10)
 
-
-
 day_off = tkinter.Label (text="day_off")
 day_off.grid(row=0, column=5, padx=10, pady=10)
 
@@ -21,19 +19,6 @@ dev_build.grid(row=1, column=5, padx=10, pady=10)
 
 find_issue = tkinter.Label (text="find_issue")
 find_issue.grid(row=2, column=5, padx=10, pady=10)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
