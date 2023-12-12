7859953a1a7edfa5228482924d1d12bbcd6ae95c
Jang eo young <jjangeoyoung@gmail.com>
2023-12-12 11:19:04 +0900

    Update main.py

---
+++ flask_app/231205_Django/main.py
@@ -0,0 +1,17 @@
+# @app.route('/')
+# def hello_world():
+#     return '<h1 style="text-align: center"> Hello World!</h1>'\
+#         '<p style="text-align: center" >hi</p>'\
+#             '<img src="">'
+
+# @app.route('/bye')
+# def bye():
+#     return 'bye'
+
+# @app.route('/<name>/<int:number>')
+# def greet(name, number):
+#     return f"hello {name}, you are {number} years old"
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+    
