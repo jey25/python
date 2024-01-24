c9f55a8ecbb5116ecf2b2761ecf4df61b00e9758
Jang eo young <jjangeoyoung@gmail.com>
2024-01-24 10:39:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -470,3 +470,42 @@ import time
 # print(a.name)
 
 
+# class Stock:
+#     def __init__(self, name, code, per, pbr, dividend):
+#         self.name = name
+#         self.code = code
+#         self.per = per
+#         self.pbr = pbr
+#         self.dividend = dividend
+
+#     def set_name(self, name):
+#         self.name = name
+
+#     def set_code(self, code):
+#         self.code = code
+
+#     def get_name(self):
+#         return self.name
+
+#     def get_code(self):
+#         return self.code
+
+#     def set_per(self, per):
+#         self.per = per
+
+#     def set_pbr(self, pbr):
+#         self.pbr = pbr
+
+#     def set_dividend(self, dividend):
+#         self.dividend = dividend
+
+# 삼성 = Stock("삼성전자", "005930", 15.79, 1.33, 2.83)
+# 현대 = Stock("현대전자", "023430", 8.79, 0.33, 4.83)
+# LG = Stock("LG", "005234", 315.79, 1.63, 1.83)
+
+# s_list = [삼성, 현대, LG]
+
+# for i in s_list:
+#     print(i.code, i.per)
+
+
