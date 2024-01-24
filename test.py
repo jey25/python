ea198ae7c90c3369dc0efa48bb52e9ae4e2f3e42
Jang eo young <jjangeoyoung@gmail.com>
2024-01-24 16:54:45 +0900

    Update test.py

--- test.py
+++ test.py
@@ -660,3 +660,14 @@ from typing import Any
 #         print(e)
 
 
+# per = ["10.31", "", "8.00"]
+
+# for i in per:
+#     try:
+#         print(float(i))
+#     except:
+#         print(0)
+#     else:
+#         print("clean data")
+#     finally:
+#         print("변환 완료")
