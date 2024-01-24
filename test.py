8d4662b8b5fd901fa4cb5859c5172a0ca106b86f
Jang eo young <jjangeoyoung@gmail.com>
2024-01-24 10:46:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -509,3 +509,27 @@ import time
 #     print(i.code, i.per)
 
 
+# 은행 계좌 개설 클래스
+# import random
+
+# class Account:
+#     def __init__(self, name, balance):
+#         self.name = name
+#         self.balance = balance
+#         self.bank = "SC은행"
+#         num1 = random.randint(0, 999)
+#         num2 = random.randint(0, 99)
+#         num3 = random.randint(0, 999999)
+
+#         num1 = str(num1).zfill(3)      # 1 -> '1' -> '001'
+#         num2 = str(num2).zfill(2)      # 1 -> '1' -> '01'
+#         num3 = str(num3).zfill(6)      # 1 -> '1' -> '0000001'
+#         self.account_number = num1 + '-' + num2 + '-' + num3  # 001-01-000001
+
+# kim = Account("김민수", 100)
+# print(kim.name)
+# print(kim.balance)
+# print(kim.bank)
+# print(kim.account_number)
+
+
