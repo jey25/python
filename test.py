40254cd04e959a0e6e8be4dcb8c430a43d62cf3c
Jang eo young <jjangeoyoung@gmail.com>
2024-01-24 12:16:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -648,3 +648,15 @@ from typing import Any
 #     new_per.append(v)
 
 # print(new_per)
+
+
+# # list 인덱싱 에러
+# data = [1, 2, 3]
+
+# for i in range(5):
+#     try:
+#         print(data[i])
+#     except IndexError as e:
+#         print(e)
+
+
