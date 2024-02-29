2efd104eb765c04320ebdb52048de570712fd36b
Jang eo young <jjangeoyoung@gmail.com>
2024-02-29 15:15:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -776,5 +776,30 @@ from typing import Any
 # print(data)
 
 
+# movie_rank = ['닥터 스트레인지', '스플릿', '럭키']
+
+# movie_rank.append("배트맨")
+# print(movie_rank)
+
+# # insert(인덱스, 원소)
+# movie_rank.insert(1, "슈퍼맨")
+# print(movie_rank)
+
+# del movie_rank[3]
+# print(movie_rank)
+
+# del movie_rank[2]
+# del movie_rank[2]
+# print(movie_rank)
+
+
+# lang1 = ["C", "C++", "JAVA"]
+# lang2 = ["Python", "Go", "C#"]
+
+# print(lang1+lang2)
+
+
+# cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+# print(len(cook))
 
 
