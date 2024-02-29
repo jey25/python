014856db9f4500c0ab25e888eea8346948628649
Jang eo young <jjangeoyoung@gmail.com>
2024-02-29 15:51:33 +0900

    Update test.py

--- test.py
+++ test.py
@@ -844,3 +844,9 @@ from typing import Any
 # t = ('A', 'b', 'c')
 # print(t)
 
+# interest = ('삼성전자', 'LG전자', 'SK Hynix')
+# print(list(interest))
+
+# interest = ['삼성전자', 'LG전자', 'SK Hynix']
+# print(tuple(interest))
+
