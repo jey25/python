601cc19d4828fea78f296af124638c9de77ff295
Jang eo young <jjangeoyoung@gmail.com>
2024-02-29 15:30:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -803,3 +803,22 @@ from typing import Any
 # print(len(cook))
 
 
+# nums = [1, 2, 3, 4, 5]
+# print(sum(nums)/len(nums))
+
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(nums[::2])
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(nums[1::2])
+
+
+# nums = [1, 2, 3, 4, 5]
+# print(nums[::-1])
+
+
+# interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+# print(" ".join(interest))
+
+
