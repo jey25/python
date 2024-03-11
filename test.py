d8a363617b16803b17610d23849a4062b9963e9a
Jang eo young <jjangeoyoung@gmail.com>
2024-03-11 16:44:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -879,3 +879,10 @@ from typing import Any
 # keys = ("apple", "pear", "peach")
 # vals = (300, 250, 400)
 # print(dict(zip(keys, vals)))
+
+# date = ['09/05', '09/06', '09/07', '09/08', '09/09']
+# close_price = [10500, 10300, 10100, 10800, 11000]
+
+# close_table = dict(zip(date, close_price))
+# print(close_table)
+
