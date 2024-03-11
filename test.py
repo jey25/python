25c7026b7b0a8b90951fc36eee1f035236460f28
Jang eo young <jjangeoyoung@gmail.com>
2024-03-11 15:06:35 +0900

    Update test.py

--- test.py
+++ test.py
@@ -857,4 +857,10 @@ from typing import Any
 # print(data)
 
 
+# # 튜플의 데이터 언패킹
+# scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
+# *valid_score, _, _= scores
+# print(valid_score)
+
+
 
