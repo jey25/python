6a84608a8b066f7ee36ea2ed46c9d3c78ae203bd
Jang eo young <jjangeoyoung@gmail.com>
2024-03-20 11:37:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -909,7 +909,13 @@ from typing import Any
 #     print("정각 아님")
 
 
+# fruit = ["사과", "포도", "홍시"]
+# user = input("좋아하는 과일은?")
 
+# if user in fruit:
+#     print("정답")
+# else:
+#     print("오답")
 
 
 
