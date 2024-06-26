ee84e9a51fc05089ca78083c1ce5a75d7da3b130
Jang eo young <jjangeoyoung@gmail.com>
2024-06-26 17:29:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1102,26 +1102,37 @@ from typing import Any
 # del movie_rank[2]
 # print(movie_rank)
 
+# cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+# print(len(cook))
 
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(nums[::2])
+# print(nums[1::2])
+# print(nums[::-1])
 
+# interest = ['삼성전자', 'LG전자', 'Naver']
+# print(interest[0], interest[2])
 
+# interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+# print(" ".join(interest))
+# print("/".join(interest))
+# print("\n".join(interest))
 
+# string = "삼성전자/LG전자/Naver"
+# t = string.split("/")
+# print(t)
 
+# data = [2, 4, 3, 1, 5, 10, 9]
+# # data.sort()
+# data2 = sorted(data)
+# print(data2)
 
+# my_variable = ()
+# print(type(my_variable))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # 하나의 데이터로 튜플 만들기
+# movie_rank = (1,)
+# print(type(movie_rank))
 
 
 
