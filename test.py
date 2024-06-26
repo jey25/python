d194a105601f95a0dde9522196c07d5ea6e9ca75
Jang eo young <jjangeoyoung@gmail.com>
2024-06-26 16:40:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1094,6 +1094,13 @@ from typing import Any
 # data = "039490     "
 # print(data.rstrip())
 
+# movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+# movie_rank.append("배트맨")
+# movie_rank.insert(1, "슈퍼맨")
+# del movie_rank[3]
+# del movie_rank[2]
+# del movie_rank[2]
+# print(movie_rank)
 
 
 
