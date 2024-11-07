8b9bca8acaee41030147983a9d95b691655ec239
Jang eo young <jjangeoyoung@gmail.com>
2024-11-07 10:59:11 +0900

    Update main.py

--- 241104_Django4/main.py
+++ 241104_Django4/main.py
@@ -32,36 +32,64 @@
 # print(human_age)
 
 
-# 상속에 대한 연습
-class Person():
+# # 상속에 대한 연습
+# class Person():
     
-    def __init__(self, first_name, last_name) -> None:
-        self.first_name = first_name
-        self.last_name = last_name
+#     def __init__(self, first_name, last_name) -> None:
+#         self.first_name = first_name
+#         self.last_name = last_name
         
-    def hello(self):
-        print("Hello")
+#     def hello(self):
+#         print("Hello")
         
-    def report(self):
-        print(f"{self.first_name} {self.last_name}")
+#     def report(self):
+#         print(f"{self.first_name} {self.last_name}")
 
-class Agent(Person):
+# class Agent(Person):
     
-    # 부모 클래스의 속성에 추가 속성을 더 부여하는 방법
-    def __init__(self, first_name, last_name, code_name) -> None:
-        Person.__init__(self, first_name, last_name)
-        self.code_name = code_name
+#     # 부모 클래스의 속성에 추가 속성을 더 부여하는 방법
+#     def __init__(self, first_name, last_name, code_name) -> None:
+#         Person.__init__(self, first_name, last_name)
+#         self.code_name = code_name
     
-    # 부모 클래스의 메서드를 오버라이트
-    def report(self):
-        print("Here")
+#     # 부모 클래스의 메서드를 오버라이트
+#     def report(self):
+#         print("Here")
     
-    def reveal(self, passcode):
-        if passcode == 123:
-            print("Pass")
-        else:
-            self.report()
+#     def reveal(self, passcode):
+#         if passcode == 123:
+#             print("Pass")
+#         else:
+#             self.report()
 
-x = Agent("jang", "ey", "x")
-x.reveal(12)
-print(x.code_name)
+# x = Agent("jang", "ey", "x")
+# x.reveal(12)
+# print(x.code_name)
+
+
+# # 상속 테스트
+# class Animal():
+#     def __init__(self, name, species):
+#         self.name = name
+#         self.species = species
+    
+#     def greet(self):
+#         print("hi, {self.name}{self.species}")
+
+
+# class Cat(Animal):
+#     def __init__(self, name):
+#         Animal.__init__(self, name, "Cat")
+    
+#     def sound(self):
+#         print("cuff")
+        
+# class Dog(Animal):
+    
+#     def __init__(self, name):
+#         Animal.__init__(self, name, "Dog")
+        
+#     def sound(self):
+#         print("ruff")
+
+
