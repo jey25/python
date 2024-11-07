96c267c47c69002a896ff0e4c0bd57c59582ba97
Jang eo young <jjangeoyoung@gmail.com>
2024-11-07 10:44:48 +0900

    Update main.py

--- 241104_Django4/main.py
+++ 241104_Django4/main.py
@@ -18,15 +18,50 @@
 # print(mycircle.perimeter())
 
 
-class Dog():
-    def __init__(self, name, breed, age):
-        self.name = name
-        self.breed = breed
-        self.age = age
+# class Dog():
+#     def __init__(self, name, breed, age):
+#         self.name = name
+#         self.breed = breed
+#         self.age = age
     
-    def calculate_human_age(self):
-        return self.age*7
+#     def calculate_human_age(self):
+#         return self.age*7
 
-hans = Dog("Hans", "Sheperd", 7)
-human_age = hans.calculate_human_age()
-print(human_age)
+# hans = Dog("Hans", "Sheperd", 7)
+# human_age = hans.calculate_human_age()
+# print(human_age)
+
+
+# 상속에 대한 연습
+class Person():
+    
+    def __init__(self, first_name, last_name) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        
+    def hello(self):
+        print("Hello")
+        
+    def report(self):
+        print(f"{self.first_name} {self.last_name}")
+
+class Agent(Person):
+    
+    # 부모 클래스의 속성에 추가 속성을 더 부여하는 방법
+    def __init__(self, first_name, last_name, code_name) -> None:
+        Person.__init__(self, first_name, last_name)
+        self.code_name = code_name
+    
+    # 부모 클래스의 메서드를 오버라이트
+    def report(self):
+        print("Here")
+    
+    def reveal(self, passcode):
+        if passcode == 123:
+            print("Pass")
+        else:
+            self.report()
+
+x = Agent("jang", "ey", "x")
+x.reveal(12)
+print(x.code_name)
