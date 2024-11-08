44fd6fda57444fb4760bccc54372f9e632fa7c02
Jang eo young <jjangeoyoung@gmail.com>
2024-11-08 10:39:30 +0900

    update 241108

--- 241107_first_web/my_site/my_site/urls.py
+++ 241107_first_web/my_site/my_site/urls.py
@@ -21,6 +21,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view),
-    path('first_app/', include('first_app.urls'))
+    path('first_app/', include('first_app.urls')),
+    path('', views.home_view)
 ]
