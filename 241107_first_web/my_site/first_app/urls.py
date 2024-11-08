da0dd442ff9bf5acb5f183b2711a130e5eb81463
Jang eo young <jjangeoyoung@gmail.com>
2024-11-08 15:22:10 +0900

    update django

--- 241107_first_web/my_site/first_app/urls.py
+++ 241107_first_web/my_site/first_app/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.first_view),
-    path('<int:num_page>', views.num_page_view),
-    path('<str:topic>/', views.new_view)
+    path('', views.simple_view)
 ]
--- 241107_first_web/my_site/first_app/views.py
+++ 241107_first_web/my_site/first_app/views.py
@@ -1,29 +1,7 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
-
-articles = {
-    'contents':'History page',
-    'test':'etc page'
-}
-
-def first_view(request):
-    return HttpResponse("first page")
-
-def new_view(request, topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(result)
-    except:
-        # result = "No page for that topic!"
-        # return HttpResponseNotFound(result)
-        raise Http404("404 GECERIC ERROR")
-
-
-# 사용자가 잘못된 topic 입력 시 redirection
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys())
-    topic = topics_list[num_page]
-
-    return HttpResponseRedirect(topic)
+def simple_view(request):
+    return render(request, 'first_app/test.html')
